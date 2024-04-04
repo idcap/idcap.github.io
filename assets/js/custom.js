7bb936c17ef95c043e29cd3343cba8414b3a3a12
Brett Dulan <brett.dulan@outlook.com>
2024-04-04 12:14:42 -0400

    Upload Assets folder

---
+++ assets/js/custom.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('.customer-text').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        centerMode: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 1
+            }
+        }]
+    });
+});
