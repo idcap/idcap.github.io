2edfa33ec2628b4b542ff3d487453ac9478e7315
Brett Dulan <bdulan715@gmail.com>
2024-04-07 18:27:40 -0400

    Updates and form submission

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -20,4 +20,28 @@ $(document).ready(function(){
             }
         }]
     });
+    $('#consultationForm').submit(function(e){
+        e.preventDefault(); // Prevent form submission
+        
+        // Get form data
+        var formData = $(this).serialize();
+        console.log(formData);
+        // Send form data via AJAX
+        $.ajax({
+            type: 'POST',
+            url: 'https://formspree.io/f/mleqogvy', // Replace 'send_email.php' with your server-side script to handle email sending
+            data: formData,
+            success: function(response){
+                // Display success message or handle response accordingly
+                alert('Form submitted successfully!');
+            },
+            error: function(xhr, status, error){
+                // Handle errors
+                console.error(xhr.responseText);
+                if (!xhr.responseText){
+                    alert('Thank you for reaching out to us!');
+                }
+            }
+        });
+    });
 });
